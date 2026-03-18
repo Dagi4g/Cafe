e45f6160ab2d8a9002811bb76ad13e895c7de442
Dagi4g <dagimgenene1@gmail.com>
2026-03-18 21:36:45 +0300

    created the menu model, updated the home.html file and the view and know the view renders from the menu.

--- order/models.py
+++ order/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    discription = models.TextField(null=True,blank=True)
+    price = models.PositiveIntegerField()
+
--- order/views.py
+++ order/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Menu
 
 def index(request):
-    return render(request, "order/home.html")
+    menu_list = Menu.objects.all()
+    context = {'menu_list':menu_list}
+    return render(request, "order/home.html", context)
