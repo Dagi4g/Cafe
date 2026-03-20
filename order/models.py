9b2ed3fbdf0ee8a84db5fab582d3519c5c0c4969
Dagi4g <dagimgenene1@gmail.com>
2026-03-20 22:16:17 +0300

    the site works in html , needs javascript and css

--- order/models.py
+++ order/models.py
@@ -4,4 +4,18 @@ class Menu(models.Model):
     name = models.CharField(max_length=200)
     discription = models.TextField(null=True,blank=True)
     price = models.PositiveIntegerField()
+    CURRENCY_CHOICES = [
+            ('USD', 'US Dollar'),
+            ('EUR', 'Euro'),
+            ('GBP', 'British Pound'),
+            ('ETB', 'Ethiopian Birr'),
+            ('JPY', 'Japanese Yen'),
+            ('CNY', 'Chinese Yuan'),
+            ('INR', 'Indian Rupee'),
+            ('AED', 'UAE Dirham'),
+            ('SAR', 'Saudi Riyal'),
+            ('KES', 'Kenyan Shilling'),
+            ('ZAR', 'South African Rand'),
+            ]
+    currency = models.CharField(max_length=30, choices=CURRENCY_CHOICES)
 
